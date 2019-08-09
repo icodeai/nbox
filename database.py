b085c80cb8097d541a9f3fed2deb61e43410ff7a
allankiplangat <allankiplangat22@outlook.com>
2019-08-09 14:05:36 +0300

    Postgres Abstract Base Class

---
+++ database.py
@@ -0,0 +1,50 @@
+from abc import (ABC, abstractmethod)
+import psycopg2
+
+
+class Postgres(ABC):
+
+    @abstractmethod
+    def connect(user, password, host, port, database):
+        pass
+
+    @abstractmethod
+    def session():
+        autocommit = True
+        pass
+
+    @abstractmethod
+    def create_database():
+        pass
+
+    @abstractmethod
+    def status():
+        pass
+
+    @abstractmethod
+    def cursor(query):
+        pass
+
+    @abstractmethod
+    def select_table(query):
+        pass
+
+    @abstractmethod
+    def create_table(query):
+        pass
+
+    @abstractmethod
+    def insert_rows(query):
+        pass
+
+    @abstractmethod
+    def show_table(query):
+        pass
+
+    @abstractmethod
+    def drop_table(query):
+        pass
+
+    @abstractmethod
+    def close():
+        pass
