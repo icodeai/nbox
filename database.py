38d3de2524ede30b9d1bec8b3537dfa89c94c4eb
Mukuzi <damukuzi@gmail.com>
2019-08-11 20:14:44 +0300

    initial commit

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from abc import (ABC, abstractmethod)
 import psycopg2
 
 
-class Postgres(ABC):
+class databaseall(ABC):
 
     @abstractmethod
     def connect(user, password, host, port, database):
@@ -48,3 +48,79 @@ class Postgres(ABC):
     @abstractmethod
     def close():
         pass
+
+
+
+class Postgres(database):
+
+    def __init__(self):
+        self.connection = None
+        self.cursordb
+
+    def connect(self, user, password, host, port, database):
+        try:
+            self.connection = psycopg2.connect(user, password, host, port,database)
+            self.cursordb = self.connection.cursor()
+            #connection properties
+            print ( self.connection.get_dsn_parameters(),"\n")
+        
+        except (Exception, psycopg2.Error) as error :    
+            print ("Error while connecting to PostgreSQL", error)
+
+    def session(self):
+        autocommit = True
+        pass
+    
+    def create_database(self):
+        pass
+
+    def status(self):
+        #shows if there is a connection to the database
+        if self.connection:
+            print("connection exists")
+        else:
+            print("no connection")
+
+    def cursor(self,query):
+        #uses cursor to execue a query
+       self.cursordb.execute(query) 
+
+    def select_table(self,query):
+        #requires a sql statement to select required table
+        try:
+            self.cursordb(query)
+            table = self.cursordb.fetchall()
+            for row in table:
+                print(row)
+
+        except (Exception, psycopg2.Error) as error :
+            print ("Error while fetching data from PostgreSQL", error)
+
+    def create_table(self,query):
+        #requires an sql statement to create required table
+        self.cursordb.execute(query)
+        self.connection.commit()
+
+    def insert_rows(self,query,row_data):
+        #inserts a single row ,query and row data specified as arguments
+        try:
+            self.cursordb.execute(query, row_data)
+            self.connection.commit()
+        except (Exception, psycopg2.Error) as error :
+                  print("Failed to insert row ", error)
+    
+    def show_table(self,query):
+        #requires an show table sql statement
+        self.cursordb.execute(query)
+        return self.cursordb.fetchall()
+
+    def drop_table(self,query):
+        self.cursordb.execute(query)
+        self.connection.commit()
+
+    def close(self):
+        if (self.connection):
+            self.cursordb.close()
+            self.connection.close()
+        else:
+            print("no connection exists currently")
