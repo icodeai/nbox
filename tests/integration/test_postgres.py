29e7d51515392c45cff0c67758aa3a661842320f
antonnifo <antonnifo@live.com>
2019-08-14 22:29:58 +0300

    relocate tests to tests folder

---
+++ tests/integration/test_postgres.py
@@ -0,0 +1,23 @@
+import os
+from unittest import TestCase
+
+from db_config import PostgresConfig
+
+
+TEST_DATABASE_URL       = os.getenv('TEST_DATABASE_URL')
+WRONG_TEST_DATABASE_URL = os.getenv('WRONG_TEST_DATABASE_URL')
+
+
+class PostgresTestCase(TestCase):
+    
+    def setUp(self):
+        self.postgres = PostgresConfig()
+       
+    def test_make_bad_connection(self):
+        self.assertEqual(self.postgres.connect(WRONG_TEST_DATABASE_URL),
+                         'failed to connect to database.')
+    
+    def test_make_good_connection(self):
+        print(TEST_DATABASE_URL)
+        self.assertNotEqual(self.postgres.connect(TEST_DATABASE_URL),
+                            'failed to connect to database.')
