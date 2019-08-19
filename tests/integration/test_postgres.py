285f76f43fe46ab03d1b5686c72e08d623b9651b
antonnifo <antonnifo@live.com>
2019-08-19 14:21:01 +0300

    test methods to create and drop database then add tearDown method

--- tests/integration/test_postgres.py
+++ tests/integration/test_postgres.py
@@ -11,10 +11,29 @@ WRONG_TEST_DATABASE_URL = os.getenv('WRONG_TEST_DATABASE_URL')
 class PostgresTestCase(TestCase):
     
     def setUp(self):
-        self.postgres = PostgresConfig()
+        self.postgres      = PostgresConfig()
+        self.database_name = 'database_for_testing'
        
     
     def test_make_good_connection(self):
-        print(TEST_DATABASE_URL)
+        
         self.assertNotEqual(self.postgres.connect(TEST_DATABASE_URL),
                             'failed to connect to database.')
+
+    def test_drop_database(self):
+
+        self.postgres.create_database(self.database_name)
+
+        self.assertNotEqual(
+            self.postgres.drop_database(self.database_name),
+                            "failed to drop database")
+
+    def test_create_database(self):
+        
+        self.assertNotEqual(
+            self.postgres.create_database(self.database_name),
+                                          "failed to create database")
+
+    def tearDown(self):
+        self.postgres.drop_database(self.database_name)
+
