174f3c15942c60f715f77ea7bcc476106df9c05a
Kertich <kipsangkertich@gmail.com>
2019-08-22 18:07:07 +0300

    Implement test 4 create_database mthod

--- tests/integration/test_postgres.py
+++ tests/integration/test_postgres.py
@@ -12,9 +12,15 @@ class PostgresTestCase(TestCase):
     
     def setUp(self):
         self.postgres = PostgresConfig()
+        self.db_name  =  "test_db_one"
        
     
     def test_make_good_connection(self):
         print(TEST_DATABASE_URL)
         self.assertNotEqual(self.postgres.connect(TEST_DATABASE_URL),
                             'failed to connect to database.')
+
+    def test_create_database(self):
+        self.assertNotIn(self.postgres.create_database(self.db_name), 'failed to connect to database.'
+      )
+        
