ab4f8391eb2923fb2dae8f807da341a14dd2780f
Joseph Orina <orinangare@gmail.com>
2019-08-26 14:40:52 +0300

    tests refactoring

--- tests/integration/test_postgres.py
+++ tests/integration/test_postgres.py
@@ -15,9 +15,8 @@ class PostgresTestCase(TestCase):
        
     
     def test_make_good_connection(self):
-        print(TEST_DATABASE_URL)
         self.assertNotEqual(self.postgres.connect(TEST_DATABASE_URL),
-                            'failed to connect to database.')
+                            'connected.')
     def test_select_table(self):
         """
         test function to select table
